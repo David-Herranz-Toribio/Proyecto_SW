255d9c55b84094ecbd17b3db74b254bf47751563
AlonsoCampi09 <alocam01@ucm.es>
2024-04-09 09:24:39 +0200

    subo una cosa de perfil que se habia borrado

--- includes/vistas/perfil/Perfil.php
+++ includes/vistas/perfil/Perfil.php
@@ -5,33 +5,71 @@ require_once CLASSES_URL . '/Post.php';
 
 $usuario = $_GET["user"] ?? NULL;
 $favs = $_GET["favs"] ?? NULL;
-$content = showProfile($usuario, $favs);
+$content = showProfile($usuario,$favs);
 
 require_once LAYOUT_URL;
 
-function showProfile($usuario, $favs){
+function showProfile($usuario,$favs){
 
-    $SettingsImage = IMG_PATH . '/Setting_icon__.png';
-    $boton_ajuste = ''; 
+    $SettingsImage = IMG_PATH.'/Setting_icon__.png';
+    $boton_ajuste= ''; 
     if(!$usuario){
-        if (isset($_SESSION['username'])){ //Hay sesion iniciada
-            $boton_ajuste= <<<EOS
-            <p>
-            <section class="datos_perfil">
-            <a href="AjustePerfil.php">
-                <img src='$SettingsImage' alt="Modificar Perfil" height="45" width="50">
-            </a>
-            </section>
-            </p>
-            EOS;
+        if (isset($_SESSION['username'])) //Hay sesion iniciada
             $usuario = $_SESSION['username'];
-        }
     }
 
-    if($usuario) {
+    if($usuario) { 
 
-        $html= "<section class = 'datos_perfil'>"; 
-        $html .= "<h1 class = 'nombre_perfil'> Perfil de @".$usuario."</h1>";  
+        $html = "<section class = 'datos_perfil'>"; 
+
+        if(isset($_SESSION['username']) && $usuario ==  $_SESSION['username']){ //TU PERFIL
+            $boton_ajuste = <<<EOS
+            
+            <div class="datos_perfil">
+                <a href="AjustePerfil.php">
+                    <img src='$SettingsImage' alt="Modificar Perfil" height="45" width="50">
+                </a>
+            </div>
+           
+            EOS;
+            $html .= "<h1 class = 'nombre_perfil'> Tu Perfil</h1>";
+        }
+        else{ // PERFIL DE OTRO
+
+            $html .= "<h1 class = 'nombre_perfil'> Perfil de @".$usuario."</h1>";  
+            //FUNCIONALIDAD DE SEGUIR/DEJAR DE SEGUIR
+            if(isset($_SESSION['username'])){
+                $us= Usuario::buscaUsuario($_SESSION['username']); 
+
+                if($us->estaSiguiendo($usuario)){ //Comprobamos si ya seguimos al user del perfil en concreto
+                    $textoBoton= 'Dejar de seguir';
+                    $seguir= false;
+                }
+                else {
+                    $textoBoton= 'Seguir';
+                    $seguir= true; 
+                }
+
+                $rutaSeguimiento= HELPERS_PATH.'/ProcesarSeguimiento.php'; 
+                $rutaRetorno= VIEWS_PATH.'/perfil/Perfil.php'; 
+ 
+                $html .= <<<EOS
+
+                <div class= "datos_perfil"> 
+                    <form action = $rutaSeguimiento method= "post"> 
+
+                    <input type = "hidden" name= "return" value= $rutaRetorno?user=$usuario>
+                    <input type = "hidden" name = "id" value = $usuario>
+                    <input type = "hidden" name = "no_seguir/seguir" value= $seguir>
+                    <button type="submit"> $textoBoton </button>
+
+                    </form>  
+
+                </div> 
+                EOS;
+            }
+            
+        }
         $html .= $boton_ajuste; 
         $html.= "</section>"; 
 
@@ -60,7 +98,11 @@ function showProfile($usuario, $favs){
 
         }else
             $html .= "<section class = 'listaPost'> <h3> No has dado Like (&#10084) a ningún post</h3></section>";
-    }else
+
+    }
+    
+    
+    else //NO SE HA INICIADO SESION
         $html = "<h1 class = 'texto_infor'>  No estas logead@ </h1>";
 
     return $html;
