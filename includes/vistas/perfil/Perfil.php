a40daf18ff747e420a92dbe62bc1009a96820aa2
oscar <oscarf1030p@gmail.com>
2024-04-11 15:35:10 +0200

    Añadidos botones para visualizar posts, pedidos y tienda

--- includes/vistas/perfil/Perfil.php
+++ includes/vistas/perfil/Perfil.php
@@ -3,38 +3,14 @@
 require_once '../../Config.php';
 require_once HELPERS_URL . '/PerfilHelper.php';
 
-$usuario = '';
-$favs = '';
-$content = '';
-$opcion= $_POST['opcion'] ?? NULL; 
-
 
 // Obtenemos el username del perfil a visualizar (Otro usuario/Yo)
 if(isset($_SESSION['username'])){
+
     $usuario = $_GET["user"] ?? $_SESSION['username'];
+    $opcion = $_POST['opcion'] ?? NULL;
     $user = Usuario::buscaUsuario($usuario);
-    $favs = $_GET["favs"] ?? NULL;
-    $content = showProfile($user, $favs);
-     
-     switch($opcion) {
-        case NULL: 
-        
-        case 'publicados': 
-            $content .= displayPosts($user);
-            break; 
-        
-        case 'favoritos': 
-            $content .= displayFavoritePost($user); 
-            break; 
-
-        case 'pedidos': 
-            //mostrar historial de pedidos 
-            break; 
-
-        case 'productos': 
-            //mostrar tienda de usuario 
-            break; 
-    } 
+    $content = showProfile($user, $opcion);
 }
 else{
     $content = showNotLogged();
