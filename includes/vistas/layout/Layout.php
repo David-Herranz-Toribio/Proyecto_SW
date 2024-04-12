a503d89f1489c2788b638fa298aa0c997a8b0f2f
oscar <oscarf1030p@gmail.com>
2024-04-12 19:47:02 +0200

    Icono de pestaña

--- includes/vistas/layout/Layout.php
+++ includes/vistas/layout/Layout.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?=CSS_PATH?>/estilos.css">
+    <link rel="stylesheet" href="<?= CSS_PATH ?>/estilos.css">
+    <link rel="shortcut icon" href="<?=IMG_PATH ?>/2MelodyLogo.png" />
     <title> 2Music </title>
 </head>
 
