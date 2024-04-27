468b40f28cdf93ed911af86d3571d7d98b196422
IgnacioXr7 <ignacilu@ucm.es>
2024-04-27 20:24:11 +0200

    Implementacion_reproductor_de_musica

--- includes/vistas/layout/Footer.php
+++ includes/vistas/layout/Footer.php
@@ -1,3 +1,15 @@
-<footer class= "footer">
-    <div class="music_player"><p> Reproductor de música </p></div>
+<footer class="footer">
+    <audio src="" controls id="player"></audio>
+    <ul id="playlist">
+        <li><a href="https://upload.wikimedia.org/wikipedia/commons/9/9b/Queryen_-_DEMO_-_002_%281995%29_-_011_-_Flying_To_Tunis_%2830sec%29.ogg">Pista 1</a></li>
+        <li><a href="https://upload.wikimedia.org/wikipedia/en/7/79/Micromoog_Demo.ogg">Pista 2</a></li>
+        <li><a href="https://ccrma.stanford.edu/~jos/mp3/gtr-nylon22.mp3">Pista 3</a></li>
+    </ul>
+    <button class="player-button" id="prev"><img src="<?=IMG_PATH?>/PlayerImages/Prev_Button.png" height="30" width="30"></button>
+    <button class="player-button" id="rewind"><img src="<?=IMG_PATH?>/PlayerImages/Rewind_Button.png" height="30" width="30"></button>
+    <button class="player-button" id="forward"><img src="<?=IMG_PATH?>/PlayerImages/Forward_Button.png" height="30" width="30"></button>
+    <button class="player-button" id="next"><img src="<?=IMG_PATH?>/PlayerImages/Next_Button.png" height="30" width="30"></button>
+    <script>
+        playerLogic();    
+    </script>
 </footer>
---
+++ js/playerLogic.js
@@ -0,0 +1,52 @@
+function playerLogic () {
+    var currentSong = 0;
+    var playing = true;
+    $("#player")[0].src = $("#playlist li a")[0].href;
+    $("#player")[0].play();
+
+    $("#playlist li a").click(function(e){
+        e.preventDefault(); 
+        $("#player")[0].src = this;
+        $("#player")[0].play();
+        $("#playlist li").removeClass("current-song");
+        currentSong = $(this).parent().index();
+        $(this).parent().addClass("current-song");
+    });
+
+    $("#player")[0].addEventListener("ended", nextSong);
+
+    $("#prev").click(function(){
+        currentSong--;
+        if(currentSong < 0){
+            currentSong = $("#playlist li a").length - 1;
+        }
+        changeSong();
+    });
+
+    $("#next").click(function(){
+        nextSong();
+    });
+
+    $("#rewind").click(function(){
+        $("#player")[0].currentTime -= 10;
+    });
+
+    $("#forward").click(function(){
+        $("#player")[0].currentTime += 10;
+    });
+
+    function nextSong(){
+        currentSong++;
+        if(currentSong == $("#playlist li a").length){
+            currentSong = 0;
+        }
+        changeSong();
+    }
+
+    function changeSong(){
+        $("#playlist li").removeClass("current-song");
+        $("#playlist li:eq("+currentSong+")").addClass("current-song");
+        $("#player")[0].src = $("#playlist li a")[currentSong].href;
+        $("#player")[0].play();
+    }
+}
