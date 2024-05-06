74dfbc21ef95471ab5c9202cefd440c76901a170
AlonsoCampi09 <alocam01@ucm.es>
2024-05-06 15:44:22 +0200

    formulario para crear cancion CASI terminado

--- includes/classes/FormularioCrearCancion.php
+++ includes/classes/FormularioCrearCancion.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'Formulario.php';
-
+require_once 'FormularioMultimedia.php';
+require_once 'Cancion.php'; 
 
 /*
     Formulario para crear una canción
@@ -13,34 +13,39 @@ require_once 'Formulario.php';
         - Letra (opcional)
         - Archivo de audio
 */
-class FormularioCrearCancion extends Formulario{
+class FormularioCrearCancion extends FormularioMultimedia{
 
-    public function __construct(){
-        parent::__construct('formCrearCancion', ['urlRedireccion' =>  VIEWS_PATH .'/perfil/Perfil.php']);
+    private $id_artista; 
+
+    public function __construct($id_artista){
+        parent::__construct('formCrearCancion', ['urlRedireccion' =>  VIEWS_PATH .'/perfil/Perfil.php', 'enctype' => 'multipart/form-data']);
+        $this->id_artista= $id_artista; 
     }
 
     protected function generaCamposFormulario(&$datos){
 
         $html =<<<EOS
-        <form>
+            <fieldset>
+            <input type= 'hidden' name= "id_artista" value= "$this->id_artista">  
             <div class='songImageInput'>
                 <label for='songImageInput'> Subir portada: </label>
-                <input type='file' id='songImageInput' name='songImage'>
+                <input type='file' id='songImageInput' name='imagen'>
+                {$erroresCampos['imagen']}
             </div>
 
             <div class='songNameInput'>
                 <label for='songNameInput'> Nombre de la canción: </label>
-                <input type='text' id='songNameInput' name='songImage' required>
+                <input type='text' id='songNameInput' name='titulo' required>
             </div>
             
             <div class='songYearInput'>
                 <label for='songYearInput'> Año: </label>
-                <input type='text' id='songYearInput' name='songYear'>
+                <input type='text' id='songYearInput' name="fecha">
             </div>
 
             <div class='songGenres'>
                 <label for="genres"> Choose genres: </label>
-                <select id="genres" name="genres[]" multiple>
+                <select id="genres" name="tags" multiple>
                     <option value="pop"> ... </option>
                     <option value="rock"> Rock </option>
                     <option value="pop"> Pop </option>
@@ -57,19 +62,28 @@ class FormularioCrearCancion extends Formulario{
 
             <div class='songAudioFile'>
                 <label for='songAudioFileInput'> Audio audio: </label>
-                <input type='file' id='songAudioFileInput   ' name='songAudioFile'>
+                <input type='file' id='songAudioFileInput' name="ruta">
+                {$erroresCampos['musica']}
             </div>
 
             <div class='submitSong'>
                 <button> Subir canción </button>
             </div>
-        </form>
+        </fieldset> 
         EOS;
 
         return $html;
     }
 
     protected function procesaFormulario(&$datos){
+        $this->errores= [];
         
+
+        /*La portada de la cancion pasa los filtros*/ 
+        $portada= self:: compruebaImagen('songImage', '/songImages/'); 
+        $cancion= self:: compruebaCancion('ruta', ''); 
+        if(count($this->errores)===0){ //NO HAY ERRORES AL PROCESAR EL FORMULARIO
+            $cancion= SW\classes\Cancion:: crearCancion($datos);
+        }
     }
 }
