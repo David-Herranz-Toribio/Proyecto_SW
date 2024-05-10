b41ef1f363ad2b14b6ff634a3f7984f3b8cbe4cf
IgnacioXr7 <ignacilu@ucm.es>
2024-05-10 22:01:48 +0200

    Cambio_estilo_letras_footer

--- js/playerLogic.js
+++ js/playerLogic.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     var currentSong = 0;
+    var isPlaying = false; // Variable para verificar si la canción está reproduciéndose
+
     logicaPlaylist(); 
 
     //Pone en el footer la playlist que queremos escuchar
@@ -12,13 +14,12 @@ $(document).ready(function(){
         logicaPlaylist();
     }
 
-
     function logicaPlaylist(){
-     
         var playing = true;
         $("#playlist li:eq("+currentSong+")").addClass("current-song");
         $("#player")[0].src = $("#playlist li a")[currentSong].href;
         $("#player")[0].play();
+        isPlaying = true; // Establecer que la canción está reproduciéndose
         showNameSong(); 
     }
 
@@ -44,6 +45,11 @@ $(document).ready(function(){
         $("#player")[0].currentTime += 10;
     });
 
+    // Al detectar el evento de pausa, establecer que la canción no está reproduciéndose
+    $("#player")[0].addEventListener("pause", function() {
+        isPlaying = false;
+        $("#playlist .current-song a").css("animation", "none"); // Quitar la animación cuando la canción se pausa
+    });
 
     /*Al hacer click en una cancion en el perfil de un artista*/ 
     $('body').on('click', '#playSong' , function(){
@@ -54,13 +60,11 @@ $(document).ready(function(){
         $("#player")[0].play();
     }); 
 
-
     /*Al hacer click en reproducir playlist/album*/ 
     $('body').on('click', '#startPlaylist', function(){
         var idPlaylist= $(this).siblings("span")[0].innerText; 
         $.get("../../helpers/obtenerCanciones.php?idPlaylist=" + idPlaylist, changePlaylist); 
     }); 
-
 
     function nextSong(){
         currentSong++;
@@ -73,21 +77,23 @@ $(document).ready(function(){
     function changeSong(){
         $("#playlist li").removeClass("current-song");
         $("#playlist li:eq("+currentSong+")").addClass("current-song");
-        
         showNameSong(); 
-       
         logicaPlaylist();
+        
+        if (isPlaying) {
+            $("#playlist .current-song a").css("animation", "blinkColor 4s infinite");
+        }
     }
 
-
     /*Esconder todas las canciones menos la actual*/ 
-   function showNameSong(){ 
-    $("#playlist li ").each(function (index) {
-        if($(this).hasClass("current-song")){
-            $(this).show(); 
-        }
-        else $(this).hide();
+    function showNameSong(){ 
+        $("#playlist li ").each(function (index) {
+            if($(this).hasClass("current-song")){
+                $(this).show(); 
+            } else {
+                $(this).hide();
+            }
         }); 
     }
 
-}); 
+});
